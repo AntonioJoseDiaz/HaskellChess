4c0398ec07f277e549421be9276856e27341f56f
unknown <antoniojosedigo@gmail.com>
2021-12-16 00:13:19 +0100

    tablero

--- Chest.hs
+++ Chest.hs
@@ -7,16 +7,21 @@ fnP = ["PN1", "PN2", "PN3", "PN4", "PN5", "PN6", "PN7", "PN8"]
 fbP = ["PB1", "PB2", "PB3", "PB4", "PB5", "PB6", "PB7", "PB8"]
 fbQ = ["TB1", "CB1", "AB1", "KB", "QB", "AB2", "CB2", "TB2"]
 
-vacio = [" * " | x<-[1..8]]
+
+vacio :: Int -> [[String]]
+vacio n = replicate n [" * " | x<-[1..8]]
+-- función con lista por compresión
 
 listaPosiciones :: Int -> [[String]]
-listaPosiciones 0 = []
-listaPosiciones n = [fnQ, fnP] ++ replicate n vacio ++ [fbP, fbQ]
+listaPosiciones n
+    | n <=3 = []
+    |otherwise = [fnQ, fnP] ++ vacio n ++ [fbP, fbQ]
+--función con uso de case of
+
 
 tablero :: Int -> Matrix String
-
 tablero n  
     |n<=3 = fromLists [["ERROR : numero de lineas invalido, debe ser >=4"]]
     |otherwise =fromLists pos
     where pos = listaPosiciones n
-
+--función con uso de case of
